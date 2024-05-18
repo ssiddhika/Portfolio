08ea75aabfa578c59aec165bc6345a15ba21790b
Siddhika <siddhikapr@gmail.com>
2024-05-18 19:44:39 +1000

    background colour footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="bg-pink-100 py-4 mt-8">
+    <footer className="bg-base-100 py-4 mt-8">
       <div className="container mx-auto text-center">
         <p>&copy; {new Date().getFullYear()} Siddhika Prasad</p>
       </div>
