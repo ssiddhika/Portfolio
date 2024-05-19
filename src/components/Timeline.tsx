076bc8769fc756ab5fc593dd515dcc40ed6aa287
Siddhika <siddhikapr@gmail.com>
2024-05-19 16:24:16 +1000

    timeline box colour

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -8,7 +8,7 @@ const Timeline = () => {
         
         {/* Timeline Entry 1 */}
         <li>
-          <div className="timeline-start timeline-box">
+          <div className="timeline-start timeline-box bg-neutral-content">
             <h3 className="text-xl font-bold">University of Technology Sydney</h3>
             <p className="text-normal italic">Ultimo, 2022 - 2026</p>
             <br></br>
@@ -33,13 +33,13 @@ const Timeline = () => {
         {/* Timeline Entry 2 */}
         <li>
           <hr />
-          <div className="timeline-middle">
+          <div className="timeline-middle ">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
               <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" />
             </svg>
           </div>
-          <div className="timeline-end timeline-box">
-            <h3 className="text-xl font-bold">Higher School Certificate</h3>
+          <div className="timeline-end timeline-box bg-neutral-content">
+            <h3 className="text-xl font-bold ">Higher School Certificate</h3>
             <p className="text-normal italic">Thomas Hassall Anglican College, 2016 - 2021</p>
             <br></br>
             <p className="text-normal">Completed my high school education in Sydney's south west! My HSC subjects included Advanced English, Advanced Math, Extension 1 Math, Economics, Physics, and Business Studies. </p>
