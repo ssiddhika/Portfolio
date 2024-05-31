d5a19ff3a87404ecd79cb103586018ad932df375
Siddhika <siddhikapr@gmail.com>
2024-05-31 20:05:25 +1000

    text refactoring

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -1,8 +1,9 @@
 import Image from 'next/image';
 
+// src/components/AboutSection.tsx
 const AboutSection = () => {
   return (
-    <div className="bg-base-200 py-12 lg:py-16 rounded-lg shadow-xl">
+    <div id="about" className="bg-base-200 py-12 lg:py-16 rounded-lg shadow-xl">
       <div className="container mx-auto px-8 lg:px-12 max-w-screen-lg">
         <div className="flex flex-col md:flex-row items-center justify-between space-y-6 md:space-y-0">
           <div className="flex-shrink-0">
@@ -15,19 +16,19 @@ const AboutSection = () => {
             />
           </div>
           <div className="flex-1 min-w-0 md:ml-12 lg:ml-16">
-            <h2 className="text-3xl lg:text-4xl font-bold text-primary mb-4">
+            <h2 className="text-3xl lg:text-4xl font-bold text-base-content mb-4">
               Learn a bit about me!
             </h2>
-            <p className="text-lg lg:text-xl text-base-content mb-4">
+            <p className="text-lg lg:text-xl text-base-content  mb-4">
               I&apos;ve always been fascinated by technology and its potential to simplify our lives. As a software engineering and business student at the University of Technology Sydney (UTS), I&apos;m passionate about using tech to innovate and solve problems.
             </p>
-            <p className="text-lg lg:text-xl text-base-content mb-4">
+            <p className="text-lg lg:text-xl text-base-content  mb-4">
               What excites me most about this field is its power to address real-world problems and create social impact. I&apos;m dedicated to leveraging my technical knowledge, innovative thinking, and leadership skills to develop solutions that meet current needs and anticipate future challenges.
             </p>
-            <p className="text-lg lg:text-xl text-base-content mb-4">
+            <p className="text-lg lg:text-xl text-base-content  mb-4">
               Currently, in my third year of study, I&apos;m focusing on full-stack development. I love bringing the technical and visual elements of digital products to life and seeing projects through from ideation to completion.
             </p>
-            <p className="text-lg font-semibold lg:text-xl text-secondary">
+            <p className="text-lg lg:text-xl font-semibold text-secondary ">
               Feel free to reach out and say hello! 😊
             </p>
           </div>
@@ -38,3 +39,4 @@ const AboutSection = () => {
 };
 
 export default AboutSection;
+
