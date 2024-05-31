6f7799af4e2a821740eea51f79242ec5e26747e3
Siddhika <siddhikapr@gmail.com>
2024-05-31 23:30:52 +1000

    Project numbers added

--- src/pages/projects/project2.tsx
+++ src/pages/projects/project2.tsx
@@ -4,14 +4,14 @@ const Project2 = () => {
   return (
     <>
       <Head>
-        <title>Project 1 | Siddhika&apos;s Portfolio</title>
+        <title>Project 2 | Siddhika&apos;s Portfolio</title>
       </Head>
       <div className="container mx-auto px-8 lg:px-12 max-w-screen-lg py-12 lg:py-16">
         <h1 className="text-3xl lg:text-4xl font-bold text-base-content mb-8">
-          Project 1
+          Project 2
         </h1>
         <p className="text-lg lg:text-xl text-base-content mb-8">
-          Detailed description of Project 1.
+          Detailed description of Project 2.
         </p>
         {/* Add more content as needed */}
       </div>
